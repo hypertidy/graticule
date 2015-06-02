65ef4c8a041f96cf09d6d0864b4199dd717f2c96
Michael Sumner <mdsumner@gmail.com>
2015-06-03 00:08:08 +1000

    added labels, changed argument names

--- R/graticule.R
+++ R/graticule.R
@@ -18,31 +18,40 @@ buildlines <- function(x) {
 }
 
 ## from raster findMethods("isLonLat")[["character"]]
-isLonLat <- function (x)
-{
-  res1 <- grep("longlat", as.character(x), fixed = TRUE)
-  res2 <- grep("lonlat", as.character(x), fixed = TRUE)
-  if (length(res1) == 0L && length(res2) == 0L) {
-    return(FALSE)
-  }
-  else {
-    return(TRUE)
-  }
-}
+# isLonLat <- function (x)
+# {
+#   res1 <- grep("longlat", as.character(x), fixed = TRUE)
+#   res2 <- grep("lonlat", as.character(x), fixed = TRUE)
+#   if (length(res1) == 0L && length(res2) == 0L) {
+#     return(FALSE)
+#   }
+#   else {
+#     return(TRUE)
+#   }
+# }
 
 lonlatp4 <- function() {
   "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0"
 }
-#' Create a graticule.
+
+#' Create graticule lines.
 #'
-#' @param easts longitudes for meridional lines
-#' @param norths latitudes for parallel lines
-#' @param ndiscr number of discrete vertices for each segment
+#' Specify the creation of lines along meridians by specifying their placement
+#' at particular \code{lons} (longitudes) and \code{lats} (latitudes) and their extents
+#' with \code{xlim} (extent of parallel line in longitude) and \code{ylim} (extent of meridional line in latitude).
+#'
+#' Provide a valid PROJ.4 string to return the graticule lines in this projection. If this is not specified the graticule
+#' lines are returned in their original longlat / WGS84.
+#' @param lons longitudes for meridional lines
+#' @param lats latitudes for parallel lines
+#' @param nverts number of discrete vertices for each segment
 #' @param xlim maximum range of parallel lines
 #' @param ylim maximum range of meridional lines
+#' @param proj optional proj.4 string for output object
 #' @export
 #'
-#' @examples \dontrun{
+#' @examples
+#'
 #' library(rgdal)
 #' x <- as.matrix(expand.grid(x = seq(100, 240, by = 15), y = seq(-85, -30, by = 15)))
 #' prj <- "+proj=laea +lon_0=180 +lat_0=-70"
@@ -57,27 +66,35 @@ lonlatp4 <- function() {
 #' plot(px, type = "n")
 #' plot(pg2, add = TRUE)
 #'
-#' }
-graticule <- function(easts, norths, ndiscr = 60, xlim, ylim, proj = NULL) {
+#' prj <- "+proj=laea +lon_0=0 +lat_0=-90 +ellps=WGS84"
+#' xx <- c(-120, -100, -80, -60, -40); yy <- c(-65, -55, -45)
+#' g3 <- graticule(xx, yy, ylim = c(-70, -30), proj = prj)
+#' g3labs <- graticule_labels(xx, c(-65, -45), xline = -85, yline = -30, proj = prj)
+#' plot(g3)
+#' text(g3labs, lab = parse(text = g3labs$lab))
+#'
+#' @importFrom raster isLonLat
+#' @importFrom sp SpatialLinesDataFrame Line Lines SpatialLines CRS spTransform
+graticule <- function(lons, lats, nverts = 60, xlim, ylim, proj = NULL) {
   if (is.null(proj)) proj <- lonlatp4()
   proj <- as.character(proj)  ## in case we are given CRS
   trans <- FALSE
-  if (!isLonLat(proj)) trans <- TRUE
-  if (missing(easts)) {
+  if (!raster::isLonLat(proj)) trans <- TRUE
+  if (missing(lons)) {
     #usr <- par("usr")
     #if (all(usr == c(0, 1, 0, 1))) {
-      easts <- seq(-180, 180, by = 15)
+      lons <- seq(-180, 180, by = 15)
   }
-  if (missing(norths)) {
-      norths <- seq(-90, 90, by = 10)
+  if (missing(lats)) {
+      lats <- seq(-90, 90, by = 10)
   }
 
-  if (missing(xlim)) xlim <- range(easts)
-  if (missing(ylim)) ylim <- range(norths)
-  xlines <- lapply(easts, limfun, lim = ylim, meridian = TRUE)
-  ylines <- lapply(norths, limfun, lim = xlim, meridian = FALSE)
-  xs <- buildlines(xlines)
-  ys <- buildlines(ylines)
+  if (missing(xlim)) xlim <- range(lons)
+  if (missing(ylim)) ylim <- range(lats)
+  xline <- lapply(lons, limfun, lim = ylim, meridian = TRUE)
+  yline <- lapply(lats, limfun, lim = xlim, meridian = FALSE)
+  xs <- buildlines(xline)
+  ys <- buildlines(yline)
   ys$id <- ys$id + max(xs$id)
   xs$type <- "meridian"
   ys$type <- "parallel"
@@ -86,10 +103,10 @@ graticule <- function(easts, norths, ndiscr = 60, xlim, ylim, proj = NULL) {
  # proj4string(d) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
   d0 <- split(d, d$id)
   l <- vector("list", length(d0))
-  for (i in seq_along(d0)) l[[i]] <- Lines(list(Line(as.matrix(d0[[i]][, c("x", "y")]))), ID = as.character(i))
-  l <- SpatialLinesDataFrame(SpatialLines(l, proj4string = CRS(lonlatp4())),
+  for (i in seq_along(d0)) l[[i]] <- sp::Lines(list(sp::Line(as.matrix(d0[[i]][, c("x", "y")]))), ID = as.character(i))
+  l <- sp::SpatialLinesDataFrame(sp::SpatialLines(l, proj4string = sp::CRS(lonlatp4())),
                         data.frame(ID = as.character(seq_along(l))))
-  if (trans) l <- rgdal:::spTransform.SpatialLinesDataFrame(l, CRS(proj))
+  if (trans) l <- sp::spTransform(l, sp::CRS(proj))
   l
   #d0$type <- c(rep("meridian", length(unique(xs$id))), rep("parallel", length(unique(ys$id))))
 
@@ -97,42 +114,52 @@ graticule <- function(easts, norths, ndiscr = 60, xlim, ylim, proj = NULL) {
 
 #' Create graticule labels.
 #'
-#' @param easts longitudes for meridional labels
-#' @param norths latitudes for parallel labels
-#' @param xlines meridian/s for placement of parallel labels
-#' @param ylines parallel/s for placement of meridian labels
+#' Returns a set of points with labels, for plotting in conjuction with \code{\link{graticule}}.
+#'
+#' SpatialPoints are returned in the projection of \code{proj} if given, or longlat / WGS84.
+#' @param lons longitudes for meridional labels
+#' @param lats latitudes for parallel labels
+#' @param xline meridian/s for placement of parallel labels
+#' @param yline parallel/s for placement of meridian labels
+#' @param proj optional proj.4 string for output object
 #' @export
-#' @importFrom sp degreeLabelsEW degreeLabelsNS coordinates<- proj4string
+#' @importFrom sp degreeLabelsEW degreeLabelsNS coordinates<- proj4string<-
 #' @examples
-#' plot(graticule(easts = c(100, 120, 160, 180), norths = c(-80,-70,-60, -50,-45, -30),  proj = "+proj=lcc +lon_0=150 +lat_0=-80"))
-#' op <- par(xpd = NA); text(labs, lab = parse(text = labs$lab), pos = c(2, 1)[labs$islon + 1], adj = 1.2);par(op)
-graticule_labels <- function(easts, norths, xlines, ylines, proj = NULL) {
+#' xx <- c(100, 120, 160, 180)
+#' yy <- c(-80,-70,-60, -50,-45, -30)
+#' prj <- "+proj=lcc +lon_0=150 +lat_0=-80"
+#' plot(graticule(lons = xx, lats = yy,  proj = prj))
+#' labs <- graticule_labels(lons = xx, lats = yy, xline = 100, yline = -80,  proj = prj)
+#' op <- par(xpd = NA)
+#' text(labs, lab = parse(text = labs$lab), pos = c(2, 1)[labs$islon + 1], adj = 1.2)
+#' par(op)
+graticule_labels <- function(lons, lats, xline, yline, proj = NULL) {
   if (is.null(proj)) proj <- lonlatp4()
   proj <- as.character(proj)  ## in case we are given CRS
   trans <- FALSE
-  if (!raster:::isLonLat(proj)) trans <- TRUE
-  if (missing(easts)) {
+  if (!raster::isLonLat(proj)) trans <- TRUE
+  if (missing(lons)) {
     #usr <- par("usr")
     #if (all(usr == c(0, 1, 0, 1))) {
-    easts <- seq(-180, 180, by = 15)
+    lons <- seq(-180, 180, by = 15)
   }
-  if (missing(norths)) {
-    norths <- seq(-90, 90, by = 10)
+  if (missing(lats)) {
+    lats <- seq(-90, 90, by = 10)
   }
-  if (missing(xlines)) xlines <- easts
-  if (missing(ylines)) ylines <- norths
+  if (missing(xline)) xline <- lons
+  if (missing(yline)) yline <- lats
 
-  lonlabs <- expand.grid(x = easts, y = ylines)
+  lonlabs <- expand.grid(x = lons, y = yline)
   lonlabs$lab <-  degreeLabelsEW(lonlabs$x)
   lonlabs$islon <- TRUE
-  latlabs <- expand.grid(x = xlines, y = norths)
+  latlabs <- expand.grid(x = xline, y = lats)
   latlabs$lab <- degreeLabelsNS(latlabs$y)
   latlabs$islon <- FALSE
   l <- rbind(lonlabs, latlabs)
   coordinates(l) <- 1:2
   proj4string(l) <- CRS(lonlatp4())
   if (trans) {
-    l <- rgdal:::spTransform.SpatialPointsDataFrame(l, CRS(proj))
+    l <- sp::spTransform(l, CRS(proj))
   }
   l
 }
--- tests/testthat/testgraticule.R
+++ tests/testthat/testgraticule.R
@@ -2,7 +2,7 @@ context("Basic function")
 
 test_that("graticule creation is successful", {
   expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15)), is_a("SpatialLinesDataFrame"))
-  expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15), ndiscr = 20), is_a("SpatialLinesDataFrame"))
-  expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15), ndiscr = 100, xlim = c(-180, 180), ylim = c(-60, -30)), is_a("SpatialLinesDataFrame"))
+  expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15), nverts = 20), is_a("SpatialLinesDataFrame"))
+  expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15), nverts = 100, xlim = c(-180, 180), ylim = c(-60, -30)), is_a("SpatialLinesDataFrame"))
 
 })
