377e1fa84564ad9042da0a4e19acf5ddb7a60daf
Michael Sumner <mdsumner@gmail.com.com>
2015-06-14 07:37:28 +1000

    label test stub

--- tests/testthat/testgraticule.R
+++ tests/testthat/testgraticule.R
@@ -6,3 +6,7 @@ test_that("graticule creation is successful", {
   expect_that(graticule(seq(100, 240, by = 15), seq(-85, -30, by = 15), nverts = 100, xlim = c(-180, 180), ylim = c(-60, -30)), is_a("SpatialLinesDataFrame"))
 
 })
+
+test_that("labels work", {
+  expect_that(graticule_labels(seq(100, 240, by = 15), seq(-85, -30, by = 15)), is_a("SpatialPointsDataFrame"))
+})
