858a8dac5c79d4cddab0700108e9adee1fb5fdb0
Michael Sumner <Michael.Sumner@aad.gov.au>
2015-08-10 09:05:44 +1000

    pathologicule

---
+++ R/pathos.R
@@ -0,0 +1,38 @@
+
+#' Create a mesh of evenly spaced lines in another projection.
+#'
+#' @param x object to build line mesh for
+#' @param proj the other projection
+#'
+#' @return spatial object
+#' @export
+#'
+#' @examples
+#' library(maptools)
+#' data(wrld_simpl)
+#' library(raster)
+#' w <- subset(wrld_simpl, NAME == "Australia")
+#' plot(w)
+#' laea <- pathologicule(w, "+proj=laea +lon_0=147 +lat_0=-42")
+#' stere <- pathologicule(w, "+proj=stere +lon_0=147 +lat_0=-42")
+#' plot(laea, add = TRUE, col = "dodgerblue")
+#' plot(stere, add = TRUE, col = "firebrick")
+#'
+#' p <- spTransform(subset(wrld_simpl, coordinates(wrld_simpl)[,2] < -20), "+proj=stere +lat_0=-90")
+#' plot(extent(p) + 1e6, asp = 1, type = "n"); plot(p, add = TRUE)
+#' laea <- pathologicule(p, "+proj=laea +lon_0=147 +lat_0=-72")
+#' stere <- pathologicule(p, "+proj=stere +lon_0=147 +lat_0=-42")
+#' plot(laea, add = TRUE, col = "dodgerblue")
+#' plot(stere, add = TRUE, col = "firebrick")
+#' @importFrom raster projection projection<-
+pathologicule <- function(x, proj) {
+  y <- spTransform(x, proj)
+  xr <- xrange(y)
+  yr <- yrange(y)
+  xs <- seq(xr[1], xr[2], length = 15)
+  ys <- seq(yr[1], yr[2], length = 15)
+  g <- graticule(xs, ys)
+  projection(g) <- proj
+  spTransform(g, projection(x))
+}
+
